063695d24b69436cbe0ff768fc2ade70ad819e9b
jpatton638 <jpatton638@gmail.com>
2018-07-09 12:53:04 +0100

    DDCNLS-4090: Updated Microservice Bootstrap to 6.18.0

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -27,7 +27,7 @@ private object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport._
 
-  private val microserviceBootstrapVersion = "6.9.0"
+  private val microserviceBootstrapVersion = "6.18.0"
   private val domainVersion = "5.0.0"
   private val playUrlBindersVersion = "2.1.0"
   private val playReactivemongoVersion = "5.2.0"
