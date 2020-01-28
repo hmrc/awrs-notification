3bf56d017a0cd669163b34d97f122c6db2a9c1f3
jmountain-hmrc <31282623+jmountain-hmrc@users.noreply.github.com>
2020-01-28 09:59:16 +0000

    DL-2714 Dependency uplift

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -1,18 +1,3 @@
-/*
- * Copyright 2016 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import sbt._
 
@@ -31,8 +16,8 @@ private object AppDependencies {
   private val playReactivemongoVersion = "7.22.0-play-26"
   private val hmrcTestVersion = "3.9.0-play-26"
 
-  private val emailAddress = "3.2.0"
-  private val mockitoVersion = "2.27.0"
+  private val emailAddress = "3.4.0"
+  private val mockitoVersion = "2.28.2"
   private val scalatestPlusPlayVersion = "3.1.2"
   private val jSoupVersion = "1.12.1"
   private val pegdownVersion = "1.6.0"
@@ -40,7 +25,7 @@ private object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "simple-reactivemongo" % playReactivemongoVersion,
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "0.39.0",
+    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.3.0",
     "uk.gov.hmrc" %% "domain" % domainVersion,
     "uk.gov.hmrc" %% "emailaddress" % emailAddress
   )
