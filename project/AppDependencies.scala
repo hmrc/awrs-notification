27358feeb9bf573b7f8151fa603a217a534bb7a1
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-08-16 12:40:22 +0100

    DL-8403 play and bootstrap upgrade plus other updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,20 +5,20 @@ import play.sbt.PlayImport._
 private object AppDependencies {
 
   private val domainVersion = "8.1.0-play-28"
-  private val hmrcMongoVersion = "0.68.0"
+  private val hmrcMongoVersion = "0.70.0"
   private val emailAddress = "3.6.0"
-  private val mockitoVersion = "4.6.1"
+  private val mockitoVersion = "4.7.0"
   private val scalatestPlusPlayVersion = "5.1.0"
   private val jSoupVersion = "1.15.2"
   private val pegdownVersion = "1.6.0"
-  private val bootstrapPlayVersion = "5.17.0"
+  private val bootstrapPlayVersion = "5.25.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"           % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"    % bootstrapPlayVersion,
-    "uk.gov.hmrc"       %% "domain"                       % domainVersion,
-    "uk.gov.hmrc"      %% "emailaddress"                  % emailAddress
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % hmrcMongoVersion,
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % bootstrapPlayVersion,
+    "uk.gov.hmrc"       %% "domain"                    % domainVersion,
+    "uk.gov.hmrc"       %% "emailaddress"              % emailAddress
   )
 
   trait TestDependencies {
@@ -29,13 +29,13 @@ private object AppDependencies {
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test: Seq[ModuleID] = Seq(
-        "org.pegdown"            %    "pegdown"                    % pegdownVersion           % scope,
-        "com.typesafe.play"      %%   "play-test"                  % PlayVersion.current      % scope,
-        "org.mockito"            %    "mockito-core"               % mockitoVersion           % scope,
-        "org.scalatestplus.play" %%   "scalatestplus-play"         % scalatestPlusPlayVersion % scope,
-        "org.scalatestplus"      %%   "scalatestplus-mockito"      % "1.0.0-M2"               % scope,
-        "org.jsoup"              %    "jsoup"                      % jSoupVersion             % scope,
-        "uk.gov.hmrc"            %%   "bootstrap-test-play-28"     % bootstrapPlayVersion     % scope
+        "org.pegdown"            %  "pegdown"                % pegdownVersion           % scope,
+        "com.typesafe.play"      %% "play-test"              % PlayVersion.current      % scope,
+        "org.mockito"            %  "mockito-core"           % mockitoVersion           % scope,
+        "org.scalatestplus.play" %% "scalatestplus-play"     % scalatestPlusPlayVersion % scope,
+        "org.scalatestplus"      %% "scalatestplus-mockito"  % "1.0.0-M2"               % scope,
+        "org.jsoup"              %  "jsoup"                  % jSoupVersion             % scope,
+        "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapPlayVersion     % scope
       )
     }.test
   }
@@ -43,9 +43,9 @@ private object AppDependencies {
   object ITTest {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test: Seq[ModuleID] = Seq(
-        "org.pegdown"                  % "pegdown"                  % pegdownVersion % scope,
-        "com.github.tomakehurst"       %  "wiremock-jre8"           % "2.33.2" % scope,
-        "com.fasterxml.jackson.module" %% "jackson-module-scala"    % "2.13.3" % scope
+        "org.pegdown"                  %  "pegdown"              % pegdownVersion % scope,
+        "com.github.tomakehurst"       %  "wiremock-jre8"        % "2.33.2"       % scope,
+        "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.3"       % scope
       )
     }.test
   }
