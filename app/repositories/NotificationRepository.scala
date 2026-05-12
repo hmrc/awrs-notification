dc09b4defa1f4dd2881d7ce103ad30593b936193
christianmennerich <177663202+christianmennerich@users.noreply.github.com>
2026-05-12 17:16:10 +0100

    DL-18538, removes deprecetd import

--- app/repositories/NotificationRepository.scala
+++ app/repositories/NotificationRepository.scala
@@ -25,7 +25,7 @@ import play.api.Logging
 import play.api.libs.json._
 import uk.gov.hmrc.mongo.MongoComponent
 import uk.gov.hmrc.mongo.play.json.{Codecs, PlayMongoRepository}
-import uk.gov.hmrc.play.http.logging.Mdc
+import uk.gov.hmrc.mdc.Mdc
 
 import scala.concurrent.{ExecutionContext, Future}
 
